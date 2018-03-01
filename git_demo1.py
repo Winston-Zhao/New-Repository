989605b166e2a81880fd486c22355693c309a054
Winston <winstonzhao@Winstons-MacBook-Pro-4.local>
2018-03-01 16:31:58 +0800

    second commit

--- git_demo1.py
+++ git_demo1.py
@@ -1,6 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf200
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-}
+print(1)
+print(2)
+# more code here 
